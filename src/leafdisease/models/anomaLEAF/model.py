27f59a5ff7f65efb12b3388e47311e5b1265d8c8
rexcrisp <rexcrisp@outlook.com.au>
2023-10-09 22:02:30 +1100

    separate unet and autoencoder

--- src/leafdisease/models/anomaLEAF/model.py
+++ src/leafdisease/models/anomaLEAF/model.py
@@ -1,81 +1,128 @@
+import torch
+from torch import nn
 
+class ConvBlock(nn.Module):
+    def __init__(self, input_size, output_size, kernel_size=4, stride=2, padding=1, activation=True, batch_norm=True):
+        super(ConvBlock, self).__init__()
+        self.conv = nn.Conv2d(input_size, output_size, kernel_size, stride, padding)
+        self.activation = activation
+        self.lrelu = nn.LeakyReLU(0.2, True)
+        self.batch_norm = batch_norm
+        self.bn = nn.BatchNorm2d(output_size)
 
-from torch import Tensor, nn
+    def forward(self, x):
+        if self.activation:
+            out = self.conv(self.lrelu(x))
+        else:
+            out = self.conv(x)
 
-from leafdisease.utils.image import pad_nextpow2
-from leafdisease.models.components.unet import Encoder, Decoder
+        if self.batch_norm:
+            return self.bn(out)
+        else:
+            return out
 
-class Discriminator(nn.Module):
-    """Discriminator.
+class DeconvBlock(nn.Module):
+    def __init__(self, input_size, output_size, kernel_size=4, stride=2, padding=1, batch_norm=True, dropout=False):
+        super(DeconvBlock, self).__init__()
+        self.deconv = nn.ConvTranspose2d(input_size, output_size, kernel_size, stride, padding)
+        self.bn = nn.BatchNorm2d(output_size)
+        self.drop = nn.Dropout(0.5)
+        self.relu = nn.ReLU(True)
+        self.batch_norm = batch_norm
+        self.dropout = dropout
 
-        Made of only one encoder layer which takes x and x_hat to produce a score.
+    def forward(self, x):
+        if self.batch_norm:
+            out = self.bn(self.deconv(self.relu(x)))
+        else:
+            out = self.deconv(self.relu(x))
 
-    Args:
-        input_size (tuple[int, int]): Input image size.
-        num_input_channels (int): Number of image channels.
-        n_features (int): Number of feature maps in each convolution layer.
-        extra_layers (int, optional): Add extra intermediate layers. Defaults to 0.
-    """
-
-    def __init__(
-        self, input_size: tuple[int, int], num_input_channels: int, n_features: int, extra_layers: int = 0
-    ) -> None:
-        super().__init__()
-        encoder = Encoder(input_size, 1, num_input_channels, n_features, extra_layers)
-        layers = []
-        for block in encoder.children():
-            if isinstance(block, nn.Sequential):
-                layers.extend(list(block.children()))
-            else:
-                layers.append(block)
-
-        self.features = nn.Sequential(*layers[:-1])
-        self.classifier = nn.Sequential(layers[-1])
-        self.classifier.add_module("Sigmoid", nn.Sigmoid())
-
-    def forward(self, input_tensor: Tensor) -> tuple[Tensor, Tensor]:
-        """Return class of object and features."""
-        features = self.features(input_tensor)
-        classifier = self.classifier(features)
-        classifier = classifier.view(-1, 1).squeeze(1)
-        return classifier, features
+        if self.dropout:
+            return self.drop(out)
+        else:
+            return out
 
 
 class Generator(nn.Module):
-    """Generator model.
+    def __init__(self, input_dim, num_filter, output_dim):
+        super(Generator, self).__init__()
 
-    Made of an Unet architecture with skip links.
+        # Encoder
+        self.conv1 = ConvBlock(input_dim, num_filter, activation=False, batch_norm=False)
+        self.conv2 = ConvBlock(num_filter, num_filter * 2)
+        self.conv3 = ConvBlock(num_filter * 2, num_filter * 4)
+        self.conv4 = ConvBlock(num_filter * 4, num_filter * 8)
+        self.conv5 = ConvBlock(num_filter * 8, num_filter * 8)
+        self.conv6 = ConvBlock(num_filter * 8, num_filter * 8)
+        self.conv7 = ConvBlock(num_filter * 8, num_filter * 8)
+        self.conv8 = ConvBlock(num_filter * 8, num_filter * 8, batch_norm=False)
+        # Decoder
+        self.deconv1 = DeconvBlock(num_filter * 8, num_filter * 8, dropout=True)
+        self.deconv2 = DeconvBlock(num_filter * 8 * 2, num_filter * 8, dropout=True)
+        self.deconv3 = DeconvBlock(num_filter * 8 * 2, num_filter * 8, dropout=True)
+        self.deconv4 = DeconvBlock(num_filter * 8 * 2, num_filter * 8)
+        self.deconv5 = DeconvBlock(num_filter * 8 * 2, num_filter * 4)
+        self.deconv6 = DeconvBlock(num_filter * 4 * 2, num_filter * 2)
+        self.deconv7 = DeconvBlock(num_filter * 2 * 2, num_filter)
+        self.deconv8 = DeconvBlock(num_filter * 2, output_dim, batch_norm=False)
 
-    Args:
-        input_size (tuple[int, int]): Size of input data.
-        latent_vec_size (int): Dimension of latent vector produced between the first encoder-decoder.
-        num_input_channels (int): Number of channels in input image.
-        n_features (int): Number of feature maps in each convolution layer.
-        extra_layers (int, optional): Extra intermediate layers in the encoder/decoder. Defaults to 0.
-        add_final_conv_layer (bool, optional): Add a final convolution layer in the decoder. Defaults to True.
-    """
+    def forward(self, x):
+        # Encoder
+        enc1 = self.conv1(x)
+        enc2 = self.conv2(enc1)
+        enc3 = self.conv3(enc2)
+        enc4 = self.conv4(enc3)
+        enc5 = self.conv5(enc4)
+        enc6 = self.conv6(enc5)
+        enc7 = self.conv7(enc6)
+        enc8 = self.conv8(enc7)
+        # Decoder with skip-connections
+        dec1 = self.deconv1(enc8)
+        dec1 = torch.cat([dec1, enc7], 1)
+        dec2 = self.deconv2(dec1)
+        dec2 = torch.cat([dec2, enc6], 1)
+        dec3 = self.deconv3(dec2)
+        dec3 = torch.cat([dec3, enc5], 1)
+        dec4 = self.deconv4(dec3)
+        dec4 = torch.cat([dec4, enc4], 1)
+        dec5 = self.deconv5(dec4)
+        dec5 = torch.cat([dec5, enc3], 1)
+        dec6 = self.deconv6(dec5)
+        dec6 = torch.cat([dec6, enc2], 1)
+        dec7 = self.deconv7(dec6)
+        dec7 = torch.cat([dec7, enc1], 1)
+        dec8 = self.deconv8(dec7)
+        out = nn.Tanh()(dec8)
+        return out
 
-    def __init__(
-        self,
-        input_size: tuple[int, int],
-        latent_vec_size: int,
-        num_input_channels: int,
-        n_features: int,
-        extra_layers: int = 0,
-        add_final_conv_layer: bool = True,
-    ) -> None:
-        super().__init__()
-        self.encoder = Encoder(
-            input_size, latent_vec_size, num_input_channels, n_features, extra_layers, add_final_conv_layer
-        )
-        self.decoder = Decoder(input_size, latent_vec_size, num_input_channels, n_features, extra_layers)
-        self.encoder = Encoder(
-            input_size, latent_vec_size, num_input_channels, n_features, extra_layers, add_final_conv_layer
-        )
+    def normal_weight_init(self, mean=0.0, std=0.02):
+        for m in self.children():
+            if isinstance(m, ConvBlock):
+                nn.init.normal(m.conv.weight, mean, std)
+            if isinstance(m, DeconvBlock):
+                nn.init.normal(m.deconv.weight, mean, std)
 
-    def forward(self, input_tensor: Tensor) -> tuple[Tensor, Tensor, Tensor]:
-        """Return generated image and the latent vectors."""
-        latent_i = self.encoder(input_tensor)
-        gen_image = self.decoder(latent_i)
-        latent_o = self.encoder2(gen_image)
-        return gen_image, latent_i, latent_o
+class Discriminator(nn.Module):
+    def __init__(self, input_dim, num_filter, output_dim):
+        super(Discriminator, self).__init__()
+
+        self.conv1 = ConvBlock(input_dim, num_filter, activation=False, batch_norm=False)
+        self.conv2 = ConvBlock(num_filter, num_filter * 2)
+        self.conv3 = ConvBlock(num_filter * 2, num_filter * 4)
+        self.conv4 = ConvBlock(num_filter * 4, num_filter * 8, stride=1)
+        self.conv5 = ConvBlock(num_filter * 8, output_dim, stride=1, batch_norm=False)
+
+    def forward(self, x, label):
+        x = torch.cat([x, label], 1)
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.conv5(x)
+        out = nn.Sigmoid()(x)
+        return out
+
+    def normal_weight_init(self, mean=0.0, std=0.02):
+        for m in self.children():
+            if isinstance(m, ConvBlock):
+                nn.init.normal(m.conv.weight, mean, std)
--- src/leafdisease/utils/image.py
+++ src/leafdisease/utils/image.py
@@ -19,3 +19,37 @@ def pad_nextpow2(batch: Tensor) -> Tensor:
     padding_h = [math.ceil((l_dim - batch.shape[-1]) / 2), math.floor((l_dim - batch.shape[-1]) / 2)]
     padded_batch = nn.functional.pad(batch, pad=[*padding_h, *padding_w])
     return padded_batch
+
+def generate_masks(k_list, n, im_size, num_channels=3):
+    """Generates random tile masks for images
+
+    Args:
+        k_list (int): size of tiles
+        n (int): number of permutations to generate at each tile size
+        im_size (int): image dimensions (assumes square)
+        num_channels (int): number of channels for mask
+
+    Returns:
+        A list of masks
+
+    Adapated from code at https://github.com/plutoyuxie/Reconstruction-by-inpainting-for-visual-anomaly-detection/blob/main/utils/__init__.py
+    
+    """
+
+    while True:
+        Ms = []
+        for k in k_list:
+            N = im_size // k
+            rdn = np.random.permutation(N**2)
+            additive = N**2 % n
+            if additive > 0:
+                rdn = np.concatenate((rdn, np.asarray([-1] * (n - additive))))
+            n_index = rdn.reshape(n, -1)
+            for index in n_index:
+                tmp = [0 if i in index else 1 for i in range(N**2)]
+                tmp = np.asarray(tmp).reshape(N, N)
+                tmp = tmp.repeat(k, 0).repeat(k, 1)
+                # Create a 3D mask with 'num_channels' channels
+                mask_3d = np.stack([tmp] * num_channels, axis=-1)
+                Ms.append(mask_3d)
+        yield Ms
