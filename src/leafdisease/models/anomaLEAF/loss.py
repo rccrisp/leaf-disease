5d73e558de6e404a94512ab9473b019d6fbac372
rexcrisp <rexcrisp@outlook.com.au>
2023-10-15 17:06:05 +1100

    move to cpu for ciede2000 calc

--- src/leafdisease/models/anomaLEAF/loss.py
+++ src/leafdisease/models/anomaLEAF/loss.py
@@ -32,7 +32,7 @@ class CIEDE2000_Loss(nn.Module):
         img1_lab = kornia.color.rgb_to_lab(img1)
         img2_lab = kornia.color.rgb_to_lab(img2)
 
-        delE = deltaE_ciede2000(img1_lab.permute(0,2,3,1).detach().numpy(), img2_lab.permute(0,2,3,1).detach().numpy())
+        delE = deltaE_ciede2000(img1_lab.permute(0,2,3,1).cpu().detach().numpy(), img2_lab.permute(0,2,3,1).cpu().detach().numpy())
 
         return delE
 
